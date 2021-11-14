d2e4899ceabd1794f2fcf407a0f75094d5bd3cb3
mmtakeuchi <mikemtaki@gmail.com>
2021-11-13 18:11:48 -0800

    code header section

--- src/containers/whatGP3/WhatGP3.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./WhatGP3.css";
-
-const WhatGP3 = () => {
-  return <div>WhatGp3</div>;
-};
-
-export default WhatGP3;
---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./WhatGPT3.css";
+
+const WhatGPT3 = () => {
+  return <div>WhatGpt3</div>;
+};
+
+export default WhatGPT3;
