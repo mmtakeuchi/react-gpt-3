05a86769c6c2553f38be115456e6109d0739905f
mmtakeuchi <mikemtaki@gmail.com>
2021-11-13 16:11:42 -0800

    create file stucture

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGP3,
+  Header,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>GPT-3</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/whatGP3/WhatGP3.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./WhatGP3.css";
+
+const WhatGP3 = () => {
+  return <div>WhatGp3</div>;
+};
+
+export default WhatGP3;
